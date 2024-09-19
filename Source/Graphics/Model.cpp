50f9a3fcc45d519cde568c43c28719e9f7e84fcd
Alex Kougentakos <77458838+AlexKougentakos@users.noreply.github.com>
2024-09-19 19:39:25 +0200

    Updated the Index & Vertex buffers to use staging buffers

--- Source/Graphics/Model.cpp
+++ Source/Graphics/Model.cpp
@@ -61,12 +61,23 @@ namespace ili
 
 		// This just calculates how much memory we need to allocate for all the vertices of the model
 		const VkDeviceSize bufferSize = sizeof(vertices[0]) * m_VertexCount;
-		m_Device.CreateBuffer(bufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, m_VertexBuffer, m_VertexBufferMemory);
+
+		VkBuffer stagingBuffer{};
+		VkDeviceMemory stagingBufferMemory{};
+		m_Device.CreateBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
 
 		void* data;
-		vkMapMemory(m_Device.GetDevice(), m_VertexBufferMemory, 0, bufferSize, 0, &data);
+		vkMapMemory(m_Device.GetDevice(), stagingBufferMemory, 0, bufferSize, 0, &data);
 		memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-		vkUnmapMemory(m_Device.GetDevice(), m_VertexBufferMemory);
+		vkUnmapMemory(m_Device.GetDevice(), stagingBufferMemory);
+
+		m_Device.CreateBuffer(bufferSize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, m_VertexBuffer, m_VertexBufferMemory);
+
+		m_Device.CopyBuffer(stagingBuffer, m_VertexBuffer, bufferSize);
+
+		vkDestroyBuffer(m_Device.GetDevice(), stagingBuffer, nullptr);
+		vkFreeMemory(m_Device.GetDevice(), stagingBufferMemory, nullptr);
 	}
 
 	void Model::CreateIndexBuffers(const std::vector<uint32_t>& indices)
@@ -78,13 +89,24 @@ namespace ili
 
 		//Create a buffer on the GPU and allocate memory for it
 		const VkDeviceSize bufferSize = sizeof(indices[0]) * m_IndexCount;
-		m_Device.CreateBuffer(bufferSize, VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, m_IndexBuffer, m_IndexBufferMemory);
-		
+
+		VkBuffer stagingBuffer{};
+		VkDeviceMemory stagingBufferMemory{};
+		m_Device.CreateBuffer(bufferSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
+
 		void* data;
 		//Create a region on the CPU which is linked to the GPU buffer we created before
-		vkMapMemory(m_Device.GetDevice(), m_IndexBufferMemory, 0, bufferSize, 0, &data);
-		memcpy(data, indices.data(), static_cast<size_t>(bufferSize)); //Copy the indices from the CPU to the GPU
-		vkUnmapMemory(m_Device.GetDevice(), m_IndexBufferMemory); //Unmap the memory from the CPU
+		vkMapMemory(m_Device.GetDevice(), stagingBufferMemory, 0, bufferSize, 0, &data); //Copy the indices from the CPU to the GPU
+		memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+		vkUnmapMemory(m_Device.GetDevice(), stagingBufferMemory); //Unmap the memory from the CPU
+
+		m_Device.CreateBuffer(bufferSize, VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, m_IndexBuffer, m_IndexBufferMemory);
+
+		m_Device.CopyBuffer(stagingBuffer, m_IndexBuffer, bufferSize);
+
+		vkDestroyBuffer(m_Device.GetDevice(), stagingBuffer, nullptr);
+		vkFreeMemory(m_Device.GetDevice(), stagingBufferMemory, nullptr);
 	}
 
 	void Model::Bind(VkCommandBuffer commandBuffer) const
